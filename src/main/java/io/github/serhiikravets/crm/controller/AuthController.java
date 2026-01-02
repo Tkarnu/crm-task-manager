064dc592c82d0bc778d777e19a0b3f51620f27bb
Tkarnu <mr.sergeykravets@gmail.com>
2026-01-02 21:44:39 +0100

    add auth controller

---
+++ src/main/java/io/github/serhiikravets/crm/controller/AuthController.java
@@ -0,0 +1,35 @@
+package io.github.serhiikravets.crm.controller;
+
+import io.github.serhiikravets.crm.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+    @Controller
+    @RequiredArgsConstructor
+    public class AuthController {
+
+        private final UserService userService;
+
+        @GetMapping("/login")
+        public String login() {
+            return "login";
+        }
+
+        @GetMapping("/register")
+        public String registerForm() {
+            return "register";
+        }
+
+        @PostMapping("/register")
+        public String register(
+                @RequestParam String email,
+                @RequestParam String password
+        ) {
+            userService.register(email, password);
+            return "redirect:/login";
+        }
+    }
+
