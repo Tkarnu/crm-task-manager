4bf49aec3b06e611dfdd4e36aaf883782818e5d6
Tkarnu <mr.sergeykravets@gmail.com>
2026-01-02 19:20:23 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/io/github/serhiikravets/crm/CrmTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.serhiikravets.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmTaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmTaskManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/serhiikravets/crm/CrmTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.serhiikravets.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmTaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
